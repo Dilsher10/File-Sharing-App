7cff4f8050301fcec5bb1eb0b141c8f7c5da0024
dilsher10 <dilsher.dahri10@gmail.com>
2024-12-29 19:02:02 +0500

    changes

--- app/(dashboard)/(routes)/files/page.js
+++ app/(dashboard)/(routes)/files/page.js
@@ -1,10 +1,12 @@
-import { UserButton } from '@clerk/nextjs'
+"use client"
 import React from 'react'
+import AllFiles from './_components/AllFiles'
 
 const Files = () => {
   return (
-    <div>Files
-        <UserButton/>
+    <div className='pb-5 px-8 md:px:28'>
+       <h2 className='py-5 font-bold'>My Files</h2>
+       <AllFiles/>
     </div>
   )
 }
